83c4934782e0426659f04c999be3d1ea6862ae66
tony-nguyen1 <nguyentoony@gmail.com>
2021-11-25 18:51:15 +0100

    routeur.php generique

--- projet/IUT_site_e-commerce/TD4-Architecture_MVC/controller/routeur.php
+++ projet/IUT_site_e-commerce/TD4-Architecture_MVC/controller/routeur.php
@@ -1,28 +1,35 @@
 <?php
 require_once File::build_path(array("controller","ControllerVoiture.php"));
-//require_once 'ControllerVoiture.php';
+
+$controller;
+$controller_class;
+$param;
+$action;
+
+//récupération puis vérification de toutes les variables de travailles
+if (isset($_GET['controller'])) {
+    $controller = $_GET['controller'];
+} else {
+    $controller = "voiture";//valeur par défaut
+}
 if (isset($_GET['action'])) {
-    // On recupère l'action passée dans l'URL
     $action = $_GET["action"];
 } else {
     $action = 'readAll';
 }
-
 if (isset($_GET['immat'])) {
-    // On recupère l'action passée dans l'URL
     $param = $_GET["immat"];
 } else {
     $param = NULL;
 }
 
-$class_methods = get_class_methods('ControllerVoiture');
 
-if (in_array($action, $class_methods)) {
-    // Appel de la méthode statique $action de ControllerVoiture
-    ControllerVoiture::$action($param); 
+$controller_class = "Controller".ucfirst($controller);
+$class_methods = get_class_methods($controller_class);
+//vérification, la classe existe? Si oui, est-ce qu'elle a la méthode demandé?
+if (class_exists($controller_class) && in_array($action, $class_methods)) {
+    $controller_class::$action($param); 
 } else {
     ControllerVoiture::error();
 }
-
-
 ?>
