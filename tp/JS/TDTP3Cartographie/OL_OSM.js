9799676caa109fbeacc20d413d01e4134e44d020
tony-nguyen1 <nguyentoony@gmail.com>
2021-12-20 12:32:24 +0100

    marker sur carte OK

--- tp/JS/TDTP3Cartographie/OL_OSM.js
+++ tp/JS/TDTP3Cartographie/OL_OSM.js
@@ -1,58 +1,15 @@
-/*$().ready(function(){
-    console.log("debut");
-    $.getJSON("http://localhost:8888/types")
-    .then(function(data) {
-        $.when(
-            data.forEach(typeEtablissement => {
-                i = 0;
-                //console.log(typeEtablissement);
-                html = "";
-                $.getJSON("http://localhost:8888/type/"+typeEtablissement)
-                .then(function(data) {
-                    console.log(data);
-                    html="";
-                    data.forEach(etablissement => {
-                        //console.log(etablissement.nom);
-                        html += "<input type='checkbox' name='"+i+"'>"+etablissement.nom+" ";
-                        i++;
-                    });
-                    $('#points_interet').append("<h3>" + typeEtablissement + "</h3><div id='"+typeEtablissement+"'>" + html+"</div>");
-                });
-            })
-        ).then(function() {
-            console.log("fin");
-        });
-    });
-});*/
-
-/*
-$().ready(function(){
-    console.log("debut");
-    $.getJSON("http://localhost:8888/types")
-    .then(function(data) {
-        data.forEach(typeEtablissement => {
-            i = 0;
-            //console.log(typeEtablissement);
-            html = "";
-            $.when(
-                $.getJSON("http://localhost:8888/type/"+typeEtablissement)
-                    .then(function(data) {
-                        console.log(data);
-                        html="";
-                        data.forEach(etablissement => {
-                            //console.log(etablissement.nom);
-                            html += "<input type='checkbox' name='"+i+"'>"+etablissement.nom+" ";
-                            i++;
-                        });
-                        $('#points_interet').append("<h3>" + typeEtablissement + "</h3><div id='"+typeEtablissement+"'>" + html+"</div>");
-                    })
-            ).then(function() {
-                $('#points_interet').accordion({collapsible: true, heightStyle: 'content'});
-            });
-        });
-    });
+let map = new ol.Map({
+    target: 'map',
+    layers: [new ol.layer.Tile({source: new ol.source.OSM()})],
+    view: new ol.View({
+    center: ol.proj.fromLonLat([3.876716,43.61]),
+    zoom: 14
+    })
 });
-*/
+
+let listOverlay = [];
+
+
 
 $().ready(function(){
     console.log("debut");
@@ -70,8 +27,9 @@ $().ready(function(){
                     html="";
                     data.forEach(etablissement => {
                         //console.log(etablissement.nom);
-                        html += "<input type='checkbox' name='"+i+"'>"+etablissement.nom+" ";
+                        html += "<input type='checkbox' id='"+i+"'>"+etablissement.nom+" ";
                         i++;
+                        foo(etablissement.long, etablissement.lat);
                     });
                     $('#points_interet').append("<h3>" + typeEtablissement + "</h3><div id='"+typeEtablissement+"'>" + html+"</div>");
                 }));
@@ -85,28 +43,32 @@ $().ready(function(){
 
 
 
+function foo(lon, lat) {
+    let image = $("#markerProto").clone();
+    $("body").prepend(image);
+
+    let div = $("#popupProto").clone();
+    $("body").prepend(div);
+    
+    let aOverlay = new ol.Overlay({position: ol.proj.fromLonLat([lon,lat]),
+        positioning: 'center-center',
+        element: document.getElementById("markerProto")}); // element fait référence à l’image
 
 
+    map.addOverlay(aOverlay);
+    //aOverlay.getElement().style.display='block';
 
-/*
-var map = new ol.Map({
-    target: 'map',
-    layers: [new ol.layer.Tile({source: new ol.source.OSM()})],
-    view: new ol.View({
-    center: ol.proj.fromLonLat([3.876716,43.61]),
-    zoom: 14
-    })
+    listOverlay.push(aOverlay);
+}
+
+
+$('body').on("change", "input[type=checkbox]", function() {
+    let valeur = $(this).attr('id');
+    console.log("sélection de la case à cocher numéro "+valeur);
+    if ($(this).is(':checked')) { 
+        listOverlay[valeur].getElement().style.display='block';
+    } else { 
+        listOverlay[valeur].getElement().style.display='none';
+    }
 });
 
-let image = $("#markerProto").clone();
-//image.prop("display", "block");
-console.log();
-
-let aOverlay = new ol.Overlay({position: ol.proj.fromLonLat([3.876716,43.61]),
-    positioning: 'center-center',
-    element: document.getElementById("markerProto")}); // element fait référence à l’image
-
-
-map.addOverlay(aOverlay);
-aOverlay.getElement().style.display='block';
-*/
